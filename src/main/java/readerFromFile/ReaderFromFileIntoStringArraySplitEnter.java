5188431c219b9dfbb26c2684145afaf368a2fc16
Надежда Клочкова <nadzeyailyichyk@gmail.com>
2023-03-24 18:06:17 +0100

    Tests for ReaderFromFileIntoStringArraySplitEnterTest

--- src/main/java/readerFromFile/ReaderFromFileIntoStringArraySplitEnter.java
+++ src/main/java/readerFromFile/ReaderFromFileIntoStringArraySplitEnter.java
@@ -4,20 +4,29 @@ import java.util.List;
 
 public class ReaderFromFileIntoStringArraySplitEnter implements ReaderFromFileIntoStringArray {
 
+
     @Override
     public String[] readFromFile(String fileName) {
-        return fromListIntoStringArraySplitEnter(readingFromFileIntoList(fileName));
+
+        if (fileName == null && fileName == "") {
+            System.out.println("File name cannot be null or empty.");
+        }
+        return fromCharListIntoStringArraySplitEnter(readingFromFileIntoList(fileName));
+
     }
-    private String [] fromListIntoStringArraySplitEnter(List list){
+
+    public String[] fromCharListIntoStringArraySplitEnter(List list) {
 
             String string = new String();
-            for (Object ch : list) {
+        try { for (Object ch : list) {
                 string = string + ch;
             }
 
-           return string.split("\n");
+        } catch( NullPointerException ex) {
+        System.out.println("List shouldn't be null ");
     }
-
+        return string.split("\n");
+}
 
 
 }
