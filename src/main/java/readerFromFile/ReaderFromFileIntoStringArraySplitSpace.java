aea13a0545d934c26ed881d43a80af1281412c6a
Надежда Клочкова <nadzeyailyichyk@gmail.com>
2023-03-24 18:12:55 +0100

    Tests for ReaderFromFileIntoStringArraySplitSpaceTest

--- src/main/java/readerFromFile/ReaderFromFileIntoStringArraySplitSpace.java
+++ src/main/java/readerFromFile/ReaderFromFileIntoStringArraySplitSpace.java
@@ -5,15 +5,18 @@ import java.util.List;
 public class ReaderFromFileIntoStringArraySplitSpace implements ReaderFromFileIntoStringArray{
     @Override
     public String[] readFromFile(String fileName) {
-        return fromListIntoStringArraySplitSpace(readingFromFileIntoList(fileName));
+        return fromCharListIntoStringArraySplitSpace(readingFromFileIntoList(fileName));
     }
 
-    private String [] fromListIntoStringArraySplitSpace(List list){
+    public String [] fromCharListIntoStringArraySplitSpace(List list){
 
         String string = new String();
-        for (Object ch : list) {
+      try{  for (Object ch : list) {
             string = string + ch;
         }
+      } catch( NullPointerException ex) {
+          System.out.println("List shouldn't be null ");
+      }
 
         return string.split(" ");
     }
---
+++ src/main/java/readerFromFile/ReaderFromFileIntoStringArraySplitSpaceTest.java
@@ -0,0 +1,70 @@
+package readerFromFile;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReaderFromFileIntoStringArraySplitSpaceTest {
+
+    String filename;
+    String[] strings = {""};
+    ReaderFromFileIntoStringArraySplitSpace reader = new ReaderFromFileIntoStringArraySplitSpace();
+
+    @Test
+    void testReadFromNullFile() {
+
+        filename = null;
+
+        assertEquals(strings.getClass(), reader.readFromFile(filename).getClass());
+
+    }
+
+    @Test
+    void testReadFromEmptyFile() {
+
+        filename = "";
+
+        assertEquals(strings.getClass(), reader.readFromFile(filename).getClass());
+
+    }
+
+    @Test
+    void testReadFromUnrealFile() {
+
+        filename = "Unreal";
+
+        assertEquals(strings.getClass(), reader.readFromFile(filename).getClass());
+
+    }
+
+
+    @Test
+    void testFromCharListIntoStringArraySplitSpace() {
+        List<Character> characterList = new ArrayList<>();
+        characterList.add('1');
+        characterList.add(' ');
+        characterList.add('2');
+
+
+        assertEquals(true, reader.fromCharListIntoStringArraySplitSpace(characterList)[1].contains("2"));
+    }
+
+    @Test
+    void testFromNullCharListIntoStringArraySplitSpace() {
+        List<Character> characterList = null;
+
+        assertEquals(strings.getClass(), reader.fromCharListIntoStringArraySplitSpace(characterList).getClass());
+    }
+
+    @Test
+    void testFromEmptyCharListIntoStringArraySplitSpace() {
+        List<Character> characterList = new ArrayList<>();
+
+        assertEquals(strings.getClass(), reader.fromCharListIntoStringArraySplitSpace(characterList).getClass());
+    }
+
+
+}
