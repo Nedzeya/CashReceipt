522b4fbc66f8b20af964bba67fea5261f7575f15
Надежда Клочкова <nadzeyailyichyk@gmail.com>
2023-03-24 18:16:26 +0100

    Test for ReceiptInputTest

---
+++ src/main/java/receipt/Input/ReceiptInputTest.java
@@ -0,0 +1,22 @@
+package receipt.Input;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReceiptInputTest {
+    String nameOfFileReceiptInput;
+    String[] strings = {"test"};
+    class TestReceiptInput implements ReceiptInput{
+
+        @Override
+        public String[] input(String nameOfFileReceiptInput) {
+            return strings ;
+        }
+    }
+    @Test
+    void testInput() {
+        ReceiptInput receiptInput = new TestReceiptInput();
+        assertEquals(strings,receiptInput.input(nameOfFileReceiptInput));
+    }
+}
